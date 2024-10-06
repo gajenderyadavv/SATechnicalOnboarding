ef16528fd7ae0f799890f163971280099f663782
gajenderyadavv <02.y.gajender@gmail.com>
2024-10-06 12:19:05 +0530

    portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -39,19 +39,19 @@ const projectList = [
   {
     title: "EVRCS",
     description:
-      "Leveraged experience leading entrepreneurial efforts by steering a start-up project in EV sector which is towards LLP registration and seed funding. Concurrently, also navigating the inclusion into Kurukshetra University's Community Incubation Centre to foster growth. Also eading the development of a pivotal Proof of Concept by integrating IoT-based cloud solutions with a native mobile application.",
+      "Leveraged experience leading entrepreneurial efforts by steering a start-up project in EV sector which is towards LLP registration and seed funding.# Concurrently, also navigating the inclusion into Kurukshetra University's Community Incubation Centre to foster growth.# Also eading the development of a pivotal Proof of Concept by integrating IoT-based cloud solutions with a native mobile application.",
     url: "https://www.evrcs.com",
   },
   {
     title: "Cloud/Home Lab",
     description:
-      "Deployed various applications on both cloud and home lab environments, such as Kasm, Portainer, Ant-media server, Nginx (for reverse proxy) and MySQL for databases utilizing Docker and Kubernetes light version i.e K3s. Set up ESXi-Arm, DDNS-Cloudflare, NAS, and a custom DNS service on the home lab Raspberry Pi.",
+      "Deployed various applications on both cloud and home lab environments, such as Kasm, Portainer, Ant-media server, Nginx (for reverse proxy) and MySQL for databases utilizing Docker and Kubernetes light version i.e K3s. Set up ESXi-Arm, DDNS-Cloudflare, NAS, and a custom DNS service on the home lab Raspberry Pi.#",
     url: "https://red-field-00baf2300.4.azurestaticapps.net/",
   },
   {
     title: "Live Video-Streaming Server",
     description:
-      "Transmitting video from Camera and Encoder through RTMP, transcoding with FFmpeg, and securing with JavaScript-based authentication. Enhanced distribution via CloudFront and storing streams on S3 for seamless delivery of frames, scalable live streaming to audiences worldwide.",
+      "Transmitting video from Camera and Encoder through RTMP, transcoding with FFmpeg, and securing with JavaScript-based authentication.# Enhanced distribution via CloudFront and storing streams on S3 for seamless delivery of frames, scalable live streaming to audiences worldwide.#",
     url: "https://github.com/gajenderyadavv/live-video-streaming-server.git",
   },
   //  {
@@ -68,33 +68,38 @@ const projectList = [
   //  },
 ];
 
+
+
+
+
 const Portfolio = () => {
   return (
     <section className="portfolio__container" id="portfolio">
       <h2>Portfolio</h2>
-      <div  className="portfolio__container_main">
-        
-          <img
-            className="portfolio__container_image"
-            src={image}
-            alt={imageAltText}
-          />
-      
-        <div className="project__container">
-          {projectList.map((project) => (
-            <div className="box" key={project.title}>
-              <a href={project.url} target="_blank" rel="noopener noreferrer">
-                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
-              </a>
-              {project?.abc?.length>0&&<div style={{display:"flex",flexDirection:"column",gap:"20px 0px",marginTop:"20px"}}>
-                {project.abc.map(a=>{
-                  return <a href={a.url} style={{color:"rgb(78, 86, 126)"}}>{a.desc}</a>
-                })}
-                </div>}
-              <p className="small">{project.description}</p>
-            </div>
-          ))}
-        </div>
+
+
+      <div className="project__container">
+        {projectList.map((project) => (
+          <div className="box" key={project.title}>
+            <a href={project.url} target="_blank" rel="noopener noreferrer">
+              <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+            </a>
+            {project?.abc?.length > 0 && <div style={{ display: "flex", flexDirection: "column", gap: "20px 0px", marginTop: "20px" }}>
+              {project.abc.map(a => {
+                return <a href={a.url} style={{ color: "rgb(78, 86, 126)" }}>{a.desc}</a>
+              })}
+            </div>}
+            <p className="small">{
+              project.description.split('#').slice(0,-1).map((str,index)=>{
+                return (
+                  <ul key={index}>
+                    <li>{str}</li>
+                  </ul>
+                )
+              })
+            }</p>
+          </div>
+        ))}
       </div>
     </section>
   );
